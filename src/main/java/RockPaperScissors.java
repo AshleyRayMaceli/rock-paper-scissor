e27b743dcb94161a8c295136adffa11a998af33e
Ashley Maceli and Nick Lyman <nick@>
2016-04-20 11:35:09 -0700

    Add passing tests for player 1 loses and passing test for different method that announces player 1 as winner

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -5,27 +5,25 @@ public class RockPaperScissors {
   public static void main(String[] args) {}
 
     public static Boolean checkWinner(String item1, String item2) {
-      if ((item1 == "rock" && item2 == "scissors") || (item1 == "scissors" && item2 == "paper")) {
+      if ((item1.equals("rock") && item2.equals("scissors")) || (item1.equals("scissors") && item2.equals("paper")) || (item1.equals("paper") && item2.equals("rock"))) {
         return true;
       } else {
         return false;
       }
     }
 
+    public static String playRPS (String player1, String player2) {
 
+      Boolean winnerResult = checkWinner(player1, player2);
 
-    // public static String playRPS (String player1, String player2) {
-    //
-    //   checkWinner(player1, player2);
-    //
-    //   if (checkWinner("", "") == true) {
-    //     return "Player 1 wins!";
-    //   } else if (checkWinner("", "") == false) {
-    //     return "Player 2 wins!";
-    //   } else {
-    //     return "Tie";
-    //   }
-    // }
+      if (winnerResult == true) {
+        return "Player 1 wins!";
+      } else if (winnerResult == false && player1.equals(player2)) {
+        return "It's a tie";
+      } else {
+        return "Player 2 wins!";
+      }
+    }
 
     // public static String playRPS (String player1, String player2) {
     //   if (checkWinner("", "") == true) {
--- src/test/java/RockPaperScissorsTest.java
+++ src/test/java/RockPaperScissorsTest.java
@@ -15,14 +15,26 @@ public class RockPaperScissorsTest {
     assertEquals(true, testGame.checkWinner("scissors", "paper"));
   }
 
+  @Test
+  public void checkWinner_paperBeatsRock_true() {
+    RockPaperScissors testGame = new RockPaperScissors();
+    assertEquals(true, testGame.checkWinner("paper", "rock"));
+  }
+
+  @Test
+  public void checkWinner_scissorsLosesToRock_false() {
+    RockPaperScissors testGame = new RockPaperScissors();
+    assertEquals(false, testGame.checkWinner("scissors", "rock"));
+  }
+
 
   // additional tests for checkWinner method here
 
   // now test playRPS method
-  // @Test
-  // public void playRPS_recieveInputForPlayer1_expectInputString() {
-  //   RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-  //   String expected = "Player 1 wins!";
-  //   assertEquals(expected, testRockPaperScissors.playRPS("rock", "scissors"));
-  // }
+  @Test
+  public void playRPS_announceWinner_winningResultString() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    String expected = "Player 1 wins!";
+    assertEquals(expected, testRockPaperScissors.playRPS("rock", "scissors"));
+  }
 }
